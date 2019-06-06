1b31f0d04314414c43eefe176586b8f4e7adca36
Beshoy Hanna <beshoyhanna@outlook.com>
2019-06-05 20:30:39 -0400

    Development (#45)
    * Service provider refactor (#35)
    
    * Added new Hazzat Service project
    
    * Renamed view model, and implemented the first webservice method
    
    * Minor renaming
    
    * Moved a couple more service calls to the new project
    
    * Added the rest of the season service calls
    
    * Added type list service call
    
    * Added Type service calls
    
    * Added the rest of the hazzat service calls
    
    * Post refactoring cleanup
    
    * Moved common functionality to web service helper (#36)
    
    * Added missing artwork for droid
    
    * Fixing up android release
    
    * Updated iOS logo
    
    * replaced all logos
    
    * Updated artwork
    
    * Updated itunes artwork
    
    * Added 57x57
    
    * App Store Submission
    
    * Refactored to use MVVM (#38)
    
    * Added Seasons menu data binding
    
    * Added hymn count next to season
    
    * Removed old SeasonInfo collection
    
    * Converted types to data binding
    
    * Converted tunes to data binding
    
    * xaml cleanup
    
    * Initial binding changes for SectionMenu page
    
    * Added Tune & type data binding to section menu
    
    * Renamed ServiceDetails to HymnGroup
    
    * Moved NavigationType under Types namespace
    
    * Renamed NavigationType to NavigationMethod and combined navigatoinId with
    Method into new class
    
    * Initial data binding for hymn page
    
    * Changed tabbed hymn pages to be data bound
    
    * Fixed Droid crash
    
    * Moved to 64 bit
    
    * Subscribed messages before loading section view model.  Fixed no internet connection bug
    
    * Fixed no content hymns, reduced locking (#42)
    
    * Release 1.1 (#44)
    
    * Changes for UWP submission
    
    * Droid release version update
    
    * 1.2.1 published for review
    
    * Content alignment (#47)
    
    * Aligned text content into a grid like pattern
    
    * Added three column support
    
    * adjusted column widths

---
+++ Hazzat/Hazzat/Types/LanguageHtmlProperties.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hazzat.Types
+{
+    public class LanguageHtmlProperties
+    {
+        /// <summary>
+        /// Bootstrap column css class name.
+        /// </summary>
+        public string BootstrapColCssClass { get; set; }
+
+        /// <summary>
+        /// Language font css class name.
+        /// </summary>
+        public string FontCssClass { get; set; }
+
+        /// <summary>
+        /// Indicates if language is RTL.
+        /// </summary>
+        public bool IsRTL { get; set; }
+    }
+}
